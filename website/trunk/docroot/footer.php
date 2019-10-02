c84146af7d31722aac5f97bf8993ff8134aadabe
Joseph Kerby <work@joekerby.net>
2019-10-01 21:32:21 -0500

    Remove vendor and website dirs

--- website/trunk/docroot/footer.php
+++
@@ -1,8 +0,0 @@
-</div class="content">
-<div class="footer">
-    Copyright &copy; 2004-2005 <a
-    href="http://www.crt.realtors.org/">Center for REALTOR &reg;
-    Technology</a>
-  </div>
-  </body>
-</html>
